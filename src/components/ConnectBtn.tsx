d269364cdf6c02fe4a5e7ba59fa9ab153e1bd69e
AlexCPT-cpu <alexodey79@gmail.com>
2023-04-28 22:20:56 +0100

    walletconnect lib

--- src/components/ConnectBtn.tsx
+++ src/components/ConnectBtn.tsx
@@ -1,72 +1,9 @@
-import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+import { Web3Button } from "@web3modal/react";
 
 const ConnectBtn = () => {
   return (
-    <ConnectButton.Custom>
-      {({
-        account,
-        chain,
-        openAccountModal,
-        openChainModal,
-        openConnectModal,
-        authenticationStatus,
-        mounted,
-      }) => {
-        // Note: If your app doesn't use authentication, you
-        // can remove all 'authenticationStatus' checks
-        const ready = mounted && authenticationStatus !== "loading";
-        const connected =
-          ready &&
-          account &&
-          chain &&
-          (!authenticationStatus || authenticationStatus === "authenticated");
-
-        return (
-          <div
-            {...(!ready && {
-              "aria-hidden": true,
-              style: {
-                opacity: 0,
-                pointerEvents: "none",
-                userSelect: "none",
-              },
-            })}
-          >
-            {(() => {
-              if (!connected) {
-                return (
-                  <button
-                    className="bg-[#005AFF] text-white rounded-lg px-4 py-2 whitespace-nowrap"
-                    onClick={openConnectModal}
-                    type="button"
-                  >
-                    Connect Wallet
-                  </button>
-                );
-              }
-
-              if (chain.unsupported) {
-                return (
-                  <button className="bg-red-500 text-white px-4 py-2 rounded-lg text-center whitespace-nowrap" onClick={openChainModal} type="button">
-                    Wrong network
-                  </button>
-                );
-              }
-
-              return (
-                <button
-                  className="bg-[#005AFF] text-white px-4 py-2 rounded-lg text-center whitespace-nowrap"
-                  onClick={openAccountModal}
-                  type="button"
-                >
-                  {account.displayName}
-                </button>
-              );
-            })()}
-          </div>
-        );
-      }}
-    </ConnectButton.Custom>
+   <Web3Button />
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,40 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "@rainbow-me/rainbowkit/styles.css";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  EthereumClient,
+  w3mConnectors,
+  w3mProvider,
+} from "@web3modal/ethereum";
+import { Web3Modal } from "@web3modal/react";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { bsc } from "wagmi/chains";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { publicProvider } from "wagmi/providers/public";
+import { arbitrum, mainnet, polygon } from "wagmi/chains";
 import { Toaster } from "react-hot-toast";
 
-const { chains, provider } = configureChains(
-  [bsc],
-  [
-    alchemyProvider({ apiKey: "aSDT-aK1-0jAEVEOfhP4WMB_zCbhbsOX" }),
-    publicProvider(),
-  ]
-);
+const chains = [arbitrum, mainnet, polygon];
+const projectId = "523d3d83a0bcca9d8ba03a932a712917";
 
-const { connectors } = getDefaultWallets({
-  appName: "Staking Pool",
-  chains,
-});
-
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
 const wagmiClient = createClient({
   autoConnect: true,
-  connectors,
+  connectors: w3mConnectors({ projectId, version: 1, chains }),
   provider,
 });
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <App />
-        <Toaster />
-      </RainbowKitProvider>
+      <App />
+      <Toaster />
     </WagmiConfig>
+    <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
   </React.StrictMode>
 );
